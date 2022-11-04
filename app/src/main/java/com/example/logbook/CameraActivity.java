e50359149f7acd62862eadf2be4c98d8da6f42cb
Lê Chí Luân <chiluan6601@gmail.com>
2022-11-04 15:30:28 +0700

    Upload image from camera

--- app/src/main/java/com/example/logbook/CameraActivity.java
+++ app/src/main/java/com/example/logbook/CameraActivity.java
@@ -3,11 +3,14 @@ package com.example.logbook;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -15,13 +18,25 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class CameraActivity extends AppCompatActivity {
     private static final int PERMISSION_CODE = 1234;
     private static final int CAPTURE_CODE = 1001;
+
+    private FusedLocationProviderClient locationClient;
+    private final int REQUEST_PERMISSION_FINE_LOCATION = 1;
+
     ImageView imageCamera;
     Button buttonUpload;
+    TextView txtLocation;
     Uri imageUri;
 
     @Override
@@ -31,22 +46,38 @@ public class CameraActivity extends AppCompatActivity {
 
         imageCamera = findViewById(R.id.imageCamera);
         buttonUpload = findViewById(R.id.buttonUpload);
+        txtLocation = findViewById(R.id.txtLocation);
+
+        locationClient = LocationServices.getFusedLocationProviderClient(this);
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSION_FINE_LOCATION);
+            }
+        } else {
+            showLocation();
+        }
 
         buttonUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                    if(checkSelfPermission(android.Manifest.permission.CAMERA) == getPackageManager().PERMISSION_DENIED ||
+                    //System OS is Marshmallow or above
+                    if(checkSelfPermission(Manifest.permission.CAMERA) == getPackageManager().PERMISSION_DENIED ||
                             checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == getPackageManager().PERMISSION_DENIED){
                         String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
                         requestPermissions(permission, PERMISSION_CODE);
                     }
+                    //Permission already granted
                     else{
                         openCamera();
+                        showLocation();
                     }
                 }
                 else {
                     openCamera();
+                    showLocation();
                 }
              }
         });
@@ -56,8 +87,10 @@ public class CameraActivity extends AppCompatActivity {
         ContentValues values = new ContentValues();
         values.put(MediaStore.Images.Media.TITLE, "New Picture");
         values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
-        imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        values.put(MediaStore.Images.Media.LATITUDE, txtLocation.getText().toString());
+        values.put(MediaStore.Images.Media.LONGITUDE, txtLocation.getText().toString());
 
+        imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
         startActivityForResult(cameraIntent, CAPTURE_CODE);
@@ -67,6 +100,7 @@ public class CameraActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode){
+            // Camera permission
             case PERMISSION_CODE:{
                 if(grantResults.length > 0 && grantResults[0] == getPackageManager().PERMISSION_GRANTED){
                     openCamera();
@@ -76,9 +110,49 @@ public class CameraActivity extends AppCompatActivity {
                     Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
                 }
             }
+            // Location permission
+            case REQUEST_PERMISSION_FINE_LOCATION:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(CameraActivity.this, "Permission Granted!", Toast.LENGTH_LONG).show();
+                    showLocation();
+                } else {
+                    Toast.makeText(CameraActivity.this, "Permission Denied!", Toast.LENGTH_LONG).show();
+                    txtLocation.setText("Location permission not granted");
+                }
         }
     }
 
+    @SuppressLint("MissingPermission")
+    private void showLocation() {
+        locationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    txtLocation.setText("Current location is:\nLat:" + location.getLatitude() + "\nLon: " + location.getLongitude());
+                }
+            }
+        });
+
+        locationClient.getLastLocation().addOnCompleteListener(this, new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                if(task.isSuccessful()) {
+                    Location location = task.getResult();
+                    if (location != null) {
+                        txtLocation.setText("LAT:" + location.getLatitude() + "°N" + " LON: " + location.getLongitude() + "°E");
+                    }
+                    else {
+                        txtLocation.setText("Problem getting the location");
+                    }
+                }
+
+            }
+        });
+
+    }
+
+    // Display the image
     @SuppressLint("MissingSuperCall")
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
