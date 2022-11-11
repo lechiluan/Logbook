4baccdd279c8753691cf8315986daf19e1d7b15a
Lê Chí Luân <chiluan6601@gmail.com>
2022-11-12 00:33:53 +0700

    take photo form camera but location don't work

--- app/src/main/java/com/example/logbook/CameraActivity.java
+++ app/src/main/java/com/example/logbook/CameraActivity.java
@@ -1,178 +1,242 @@
 package com.example.logbook;
 
+import static android.Manifest.*;
+import static androidx.constraintlayout.motion.widget.Debug.getLocation;
+
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.ContentValues;
-import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.location.Location;
-import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
 public class CameraActivity extends AppCompatActivity {
-    private static final int PERMISSION_CODE = 1234;
-    private static final int CAPTURE_CODE = 1001;
 
-    private FusedLocationProviderClient locationClient;
-    private final int REQUEST_PERMISSION_FINE_LOCATION = 1;
-
+    Button buttonTakePhoto;
+    Button btnNext;
+    Button btnPrev;
     ImageView imageCamera;
-    Button buttonUpload;
     TextView txtLocation;
-    Uri imageUri;
+    private ImageCapture imageCapture;
+
+    private PreviewView previewView;
+    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA"}; //, "android.permission.ACCESS_FINE_LOCATION"};
+
+    private static final String FILE_NAME = "Camera.txt"; //File name for the text file
+    private int currentIndex = -1; //Index of the current image
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        findAllElements();
 
-        imageCamera = findViewById(R.id.imageCamera);
-        buttonUpload = findViewById(R.id.buttonUpload);
-        txtLocation = findViewById(R.id.txtLocation);
-
-        locationClient = LocationServices.getFusedLocationProviderClient(this);
-
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSION_FINE_LOCATION);
-            }
-        } else {
-            showLocation();
+        if (allPermissionsGranted()) {
+            startCamera();
+        }
+        else {
+            int REQUEST_CODE_PERMISSIONS = 101;
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
         }
 
-        buttonUpload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                    //System OS is Marshmallow or above
-                    if(checkSelfPermission(Manifest.permission.CAMERA) == getPackageManager().PERMISSION_DENIED ||
-                            checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == getPackageManager().PERMISSION_DENIED){
-                        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
-                        requestPermissions(permission, PERMISSION_CODE);
-                    }
-                    //Permission already granted
-                    else{
-                        openCamera();
-                        showLocation();
-                    }
-                }
-                else {
-                    openCamera();
-                    showLocation();
-                }
-             }
+        buttonTakePhoto.setOnClickListener(v -> takePhoto());
+
+        btnNext.setOnClickListener(v -> {
+            setAnimationLeftToRight();
+            setImageFromStorage();
+        });
+        btnPrev.setOnClickListener(v -> {
+            setAnimationRightToLeft();
+            setImageFromStorage();
         });
     }
 
-    public void openCamera() {
-        ContentValues values = new ContentValues();
-        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            String[] permission = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(permission, PERMISSION_CODE);
-            }
-            return;
-        }
-        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        // global variable
-        double longitude = location.getLongitude();
-        double latitude = location.getLatitude();
-
-        values.put(MediaStore.Images.Media.TITLE, "New Picture");
-        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
-        values.put(MediaStore.Images.Media.LATITUDE, latitude);
-        values.put(MediaStore.Images.Media.LONGITUDE, longitude);
-
-
-        imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-        startActivityForResult(cameraIntent, CAPTURE_CODE);
-    }
-// CAp quyền
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode){
-            // Camera permission
-            case PERMISSION_CODE:{
-                if(grantResults.length > 0 && grantResults[0] == getPackageManager().PERMISSION_GRANTED){
-                    openCamera();
-                }
-                else{
-                    //permission denied
-                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
-                }
-            }
-            // Location permission
-            case REQUEST_PERMISSION_FINE_LOCATION:
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(CameraActivity.this, "Permission Granted!", Toast.LENGTH_LONG).show();
-                    showLocation();
-                } else {
-                    Toast.makeText(CameraActivity.this, "Permission Denied!", Toast.LENGTH_LONG).show();
-                    txtLocation.setText("Location permission not granted");
-                }
-        }
-    }
 
     @SuppressLint("MissingPermission")
-    private void showLocation() {
-        locationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-            @Override
-            public void onSuccess(Location location) {
-                if (location != null) {
-                    txtLocation.setText("Current location is:\nLat:" + location.getLatitude() + "\nLon: " + location.getLongitude());
-                }
-            }
-        });
-
-        locationClient.getLastLocation().addOnCompleteListener(this, new OnCompleteListener<Location>() {
-            @Override
-            public void onComplete(@NonNull Task<Location> task) {
-                if(task.isSuccessful()) {
-                    Location location = task.getResult();
-                    if (location != null) {
-                        txtLocation.setText("LAT:" + location.getLatitude() + "°N" + " LON: " + location.getLongitude() + "°E");
-                    }
-                    else {
-                        txtLocation.setText("Problem getting the location");
-                    }
-                }
-            }
-        });
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        // check location permission here
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(allPermissionsGranted()) {
+            startCamera();
+        } else {
+            Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show();
+            finish();
+        }
     }
 
-    // Display the image
-    @SuppressLint("MissingSuperCall")
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (resultCode == RESULT_OK) {
-            imageCamera.setImageURI(imageUri);
 
+    private boolean allPermissionsGranted() {
+        for (String permission : REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
         }
+        return true;
+    }
+    
+    public void startCamera() {
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+
+        cameraProviderFuture.addListener(() -> {
+            try {
+                // Camera provider is now guaranteed to be available
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                // Set up the view finder use case to display camera preview
+                Preview preview = new Preview.Builder().build();
+                // Choose the camera by requiring a lens facing
+                CameraSelector cameraSelector = new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
+
+                // Image capture use case
+                imageCapture = new ImageCapture.Builder()
+                        .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                        .build();
+                // Connect the preview use case to the previewView
+                preview.setSurfaceProvider(previewView.getSurfaceProvider());
+                // Attach use cases to the camera with the same lifecycle owner
+                Camera camera = cameraProvider.bindToLifecycle((this), cameraSelector, preview, imageCapture);
+            } catch (InterruptedException | ExecutionException e) {
+                // handle InterruptedException.
+                Toast.makeText(this, "Error:" + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+    private List<String> getImageAbsolutePaths() {
+        final List<String> paths = new ArrayList();
+        final Uri uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        final String[] projection = { MediaStore.MediaColumns.DATA,
+                MediaStore.Images.Media.BUCKET_DISPLAY_NAME };
+        final String orderBy = MediaStore.Images.Media.DATE_TAKEN + " DESC";
+        final Cursor cursor = this.getContentResolver().query(uri, projection, null,
+                null, orderBy);
+
+        final int column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
+        while (cursor.moveToNext()) {
+            final String absolutePathOfImage = cursor.getString(column_index_data);
+            paths.add(absolutePathOfImage);
+        }
+        cursor.close();
+        return paths;
+    }
+    private void takePhoto() {
+        long timestamp = System.currentTimeMillis();
+        // get current location
+        getLocation();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.TITLE, "Image_" + timestamp);
+        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, "Image_" + timestamp);
+        contentValues.put(MediaStore.Images.Media.DESCRIPTION, "Image captured by camera");
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        contentValues.put(MediaStore.Images.Media.DATE_TAKEN, timestamp);
+        contentValues.put(MediaStore.Images.Media.DATE_ADDED, timestamp);
+        contentValues.put(MediaStore.Images.Media.DATE_MODIFIED, timestamp);
+        // put current location in the image
+//        contentValues.put(MediaStore.Images.Media.LATITUDE, locationClient.getLastLocation().getResult().getLatitude());
+//        contentValues.put(MediaStore.Images.Media.LONGITUDE, locationClient.getLastLocation().getResult().getLongitude());
+
+
+
+        ImageCapture.OutputFileOptions outputFileOptions =
+                new ImageCapture.OutputFileOptions.Builder(getContentResolver(),
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        contentValues).build();
+
+        imageCapture.takePicture(outputFileOptions, getExecutor(),
+                new ImageCapture.OnImageSavedCallback() {
+                    @Override
+                    public void onImageSaved(ImageCapture.OutputFileResults outputFileResults) {
+
+                        List<String> imageAbsolutePaths = getImageAbsolutePaths();
+                        // display recent captured photo
+                        Glide.with(CameraActivity.this).load(imageAbsolutePaths.get(0))
+                                .centerCrop()
+                                .into(imageCamera);
+                        txtLocation.setText(outputFileResults.getSavedUri().getPath());
+                        Toast.makeText(CameraActivity.this, "Photo has been saved successfully. "+imageAbsolutePaths.size()+"@"+ outputFileResults.getSavedUri().getPath(), Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(ImageCaptureException exception) {
+                        Toast.makeText(CameraActivity.this, "Error saving photo: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+    }
+    private void setImageFromStorage(){
+        List<String> imageAbsolutePaths = getImageAbsolutePaths();
+        final int count = imageAbsolutePaths.size();
+
+        if (count ==0 ) {
+            Toast.makeText(CameraActivity.this, "No photo found", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            currentIndex++;
+            if (currentIndex == count) {
+                currentIndex = 0;
+            }
+            final String imagePath = imageAbsolutePaths.get(currentIndex);
+            txtLocation.setText(imagePath);
+            Glide.with(this).load(imagePath)
+                    .centerCrop()
+                    .into(imageCamera);
+        }
+    }
+    private void setAnimationLeftToRight() {
+        // Animation using pre-defined android Slide Left-to-Right
+        Animation in_left = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
+        imageCamera.setAnimation(in_left);
+    }
+    private void setAnimationRightToLeft() {
+        // Animation using pre-defined android Slide Right-to-Left
+        Animation in_right = AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right);
+        imageCamera.setAnimation(in_right);
+    }
+    Executor getExecutor() {
+        return ContextCompat.getMainExecutor(this);
+    }
+
+    private void findAllElements() {
+        previewView = findViewById(R.id.previewView);
+        buttonTakePhoto= findViewById(R.id.buttonTakePhoto);
+        btnNext = findViewById(R.id.buttonNext);
+        btnPrev = findViewById(R.id.buttonPrevious);
+        imageCamera = findViewById(R.id.imageCamera);
+        txtLocation = findViewById(R.id.txtLocation);
     }
 }
--- app/src/main/java/com/example/logbook/MainActivity.java
+++ app/src/main/java/com/example/logbook/MainActivity.java
@@ -2,27 +2,29 @@ package com.example.logbook;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.regex.*;
 
 import com.bumptech.glide.Glide;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.ArrayList;
+
+//References
+// https://iotes.lk/2020/12/13/camerax-android-in-java/#google_vignette
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,22 +33,148 @@ public class MainActivity extends AppCompatActivity {
     ImageView imageView;
     TextView label;
 
-    ArrayList<String> savedList; // list of saved URLs
+    String regex = "(https?:\\/\\/.*\\.(?:png|jpg|gif|jpeg))"; //regex for image url
 
-    int index = -1;
+    ArrayList<String> imageURLs = new ArrayList<>(); // list of saved URLs
+
+    int currentIndex = 0; // index of current image
     // -1 means no image is currently displayed
-
-    String fileName = "file.txt"; // file name to save the list of URLs
-
-    // Regex pattern for URL validation
-    String regex = "(https?:\\/\\/.*\\.(?:png|jpg))";
-
+    private static final String FILE_NAME = "URLofImage.txt"; // file name to save the list of URLs
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // retrieve all ui elements on the form
+        findAllElements();
+        try {
+            loadURLs();
+        } catch (IOException e) {
+            e.printStackTrace();
+            displayMessage("Read file error, file = " + FILE_NAME);
+        }
+        // load URL into ImageView by using Glide
+        setImage();
+        whenClickNext();
+        whenClickPrevious();
+        whenClickAdd();
+        whenClickReset();
+        whenClickCamera();
+    }
 
-        // Initialize the views
+    private void whenClickCamera() {
+        btnCamera.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, CameraActivity.class));
+        });
+    }
+
+    private void whenClickReset() {
+        btnClear.setOnClickListener(v -> {
+            imageURLs.clear();
+            imageView.setImageResource(0);
+            removeFile();
+            currentIndex = 0;
+            displayMessage("Data reset completed");
+        });
+    }
+
+    private void removeFile() {
+        getApplicationContext().deleteFile(FILE_NAME);
+    }
+
+    private void whenClickAdd() {
+        btnAdd.setOnClickListener(v -> {
+            String URL = inputURL.getText().toString().trim();
+            Pattern p = Pattern.compile(regex);
+            Matcher m = p.matcher(URL);
+            // check if the URL is empty
+            if (URL.isEmpty()) {
+                inputURL.setError("Please enter a URL");
+                inputURL.requestFocus();
+            }
+            else {
+                if(m.matches()){
+                    imageURLs.add(URL);
+                    try {
+                        saveToFile(URL);
+                        displayMessage("URL added successfully");
+                        Glide.with(this)
+                                .load(URL)
+                                .into(imageView);
+                        inputURL.setText("");
+                        currentIndex = imageURLs.indexOf(URL);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        displayMessage("Save file error");
+                    }
+                }
+                else{
+                    inputURL.setError("Please enter a valid URL");
+                    inputURL.requestFocus();
+                }
+            }
+        });
+    }
+
+    private void displayMessage(String message) {
+        label.setText(message);
+    }
+
+
+    private void saveToFile(String url) throws IOException {
+        FileOutputStream fileOutputStream = getApplicationContext().openFileOutput(FILE_NAME, Context.MODE_APPEND);
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+        BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+        bufferedWriter.write(url);
+        bufferedWriter.newLine();
+        bufferedWriter.flush();
+        bufferedWriter.close();
+        outputStreamWriter.close();
+    }
+
+    private void whenClickPrevious() {
+        btnPrev.setOnClickListener(v -> {
+            currentIndex--;
+            setImage();
+        });
+    }
+
+    private void whenClickNext() {
+        btnNext.setOnClickListener(v -> {
+            currentIndex++;
+            setImage();
+            displayMessage("Current index = " + currentIndex);
+        });
+    }
+
+    private void loadURLs() throws IOException {
+        FileInputStream fileInputStream = getApplicationContext().openFileInput(FILE_NAME);
+        if (fileInputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String lineData =
+                    bufferedReader.readLine();
+            while (lineData != null) {
+                imageURLs.add(lineData);
+                lineData = bufferedReader.readLine();
+            }
+        }
+    }
+
+    private void setImage() {
+        int size = imageURLs.size();
+        if (currentIndex >= size) {
+            currentIndex = 0;
+        } else if (currentIndex < 0) {
+            currentIndex = size - 1;
+        }
+        if (size > 0) {
+            Glide.with(this)
+                    .load(imageURLs.get(currentIndex))
+                    .into(imageView);
+        }
+    }
+    private void findAllElements() {
+        // findViewById() for all the views
         btnAdd = findViewById(R.id.buttonAdd);
         btnPrev = findViewById(R.id.buttonPrevious);
         btnNext = findViewById(R.id.buttonNext);
@@ -55,108 +183,5 @@ public class MainActivity extends AppCompatActivity {
         inputURL = findViewById(R.id.editTextURL);
         imageView = findViewById(R.id.imageDisplay);
         label = findViewById(R.id.textNameImage);
-
-        // Sava list old data
-        savedList = readFile(fileName);
-
-        if (savedList.size() != 0) {
-            Glide.with(this).load(savedList.get(0)).into(imageView);
-        }
-
-        // Switch to Camera Activity
-        btnCamera.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, CameraActivity.class)));
-
-        // Clear the list
-        btnClear.setOnClickListener(v -> {
-            savedList.clear();
-            index = -1;
-            label.setText("");
-            imageView.setImageResource(0);
-            saveFile(fileName, savedList);
-        });
-
-        btnAdd.setOnClickListener(v -> {
-            if (!inputURL.getText().toString().trim().isEmpty()) {
-                String imageURL = inputURL.getText().toString().trim();
-
-                Pattern pattern = Pattern.compile(regex);
-                Matcher matcher = pattern.matcher(imageURL);
-
-                if (matcher.matches()) {
-                    index = savedList.indexOf(imageURL);
-                    Glide.with(this).load(imageURL).into(imageView);
-
-                    savedList.add(imageURL);
-                    inputURL.setText("");
-                    saveFile(fileName, savedList);
-                    savedList = readFile(fileName);
-                    Toast.makeText(getBaseContext(), "Add Image Successfully", Toast.LENGTH_LONG).show();
-
-                } else {
-                    inputURL.setError("Invalid Image URL");
-                    inputURL.requestFocus();
-                }
-            } else {
-                inputURL.setError("Please enter image URL");
-                inputURL.requestFocus();
-            }
-        });
-
-        btnNext.setOnClickListener(v -> {
-            int count = savedList.size();
-            if (count > 0 && count != 1) {
-                index++;
-                if (index == count) {
-                    index = 0;
-                }
-                Glide.with(this).load(savedList.get(index)).into(imageView);
-                Animation right = AnimationUtils.loadAnimation(this, R.anim.slide_in_right);
-                imageView.startAnimation(right);
-            } else {
-                Toast.makeText(getBaseContext(), "No image", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        btnPrev.setOnClickListener(v -> {
-            int count = savedList.size();
-            if (count > 0 && count != 1) {
-                index--;
-                if (index < 0) {
-                    index = count - 1;
-                }
-                Glide.with(this).load(savedList.get(index)).into(imageView);
-                Animation left = AnimationUtils.loadAnimation(this, R.anim.slide_in_left);
-                imageView.startAnimation(left);
-            } else {
-                Toast.makeText(getBaseContext(), "No image", Toast.LENGTH_LONG).show();
-            }
-        });
-    }
-
-    public void saveFile(String file, ArrayList<String> text) {
-        try {
-            FileOutputStream fos = openFileOutput(file, Context.MODE_PRIVATE);
-            ObjectOutputStream oos = new ObjectOutputStream(fos);
-
-            oos.writeObject(text);
-            oos.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-            Toast.makeText(this, "Error saving file", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    public ArrayList<String> readFile(String file) {
-        ArrayList<String> text = new ArrayList<>();
-        try {
-            FileInputStream fis = openFileInput(file); // open file
-            ObjectInputStream ois = new ObjectInputStream(fis); // read object
-            text = (ArrayList<String>) ois.readObject(); // cast to ArrayList<String>
-            ois.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-            Toast.makeText(this, "Error reading file", Toast.LENGTH_SHORT).show();
-        }
-        return text;
     }
 }
